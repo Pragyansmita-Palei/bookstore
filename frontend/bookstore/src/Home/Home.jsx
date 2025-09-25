5bacb500dd90de0f94ae414c0871b8a6ad73184b
Pragyansmita Palei <pragyansmitapalei29@gmail.com>
2025-09-25 23:06:07 +0530

    modified navbar

--- frontend/bookstore/src/Home/Home.jsx
+++ frontend/bookstore/src/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Banner from '../pages/Banner';
 import Freebook from '../pages/Freebook';
-import Navbar from '../components/navbar';
 import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 function Home() {
   return (
     <>
