1faf828ca9b3d2c931edd0bf479351fdb91a3c4e
PRAGYANSMITA PALEI <156501274+Pragyansmita-Palei@users.noreply.github.com>
2025-09-26 20:06:13 +0530

    Update Freebook.jsx

--- frontend/bookstore/src/pages/Freebook.jsx
+++ frontend/bookstore/src/pages/Freebook.jsx
@@ -13,7 +13,7 @@ function Freebook() {
    const [isMobile, setIsMobile] = useState(false);
 
   useEffect (()=>{
-    axios.get(`${BASE_URL}/book/get`)
+    axios.get(https://bookstore-ru35.onrender.com/api/V1/book)
     .then((res)=>{
       const freebook = res.data.book.filter((book)=>book.category ==="free");
      setbook(freebook);
